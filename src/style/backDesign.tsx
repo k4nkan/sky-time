6c9a15283f068b917505f8fe88b6ba116d1363d4
sub pc <cylbxt126@gmail.com>
2024-04-26 15:06:46 +0900

    4/26 progress, chenged clock design

--- src/style/backDesign.tsx
+++ src/style/backDesign.tsx
@@ -5,19 +5,47 @@ interface BackColorChangeProps {
   CurrentMinutes: number;
 }
 
+const backgroundSetup: React.CSSProperties = {
+  width: "100%",
+  height: "100vh",
+}
+
+const circleObject: React.CSSProperties = {
+  width: "200px",
+  height: "200px",
+  borderRadius: "50%",
+
+  backgroundColor: "#ccc",
+
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%,-50%)",
+
+  display: "flex",
+  fontSize: "30px",
+  justifyContent: "center",
+  alignItems: "center",
+}
+
 const BackColorChange: React.FC<BackColorChangeProps> = ({
   CurrentHours,
   CurrentMinutes,
 }) => {
-  const style: React.CSSProperties = {
-    width: "100%",
-    height: "100vh",
+  const dynamicBackgroundSetup: React.CSSProperties = {
+    ...backgroundSetup,
     backgroundColor: `rgb(0, ${CurrentMinutes}, ${CurrentHours})`,
+  };
+  const dynamicCircleObject: React.CSSProperties = {
+    ...circleObject,
+    color: `rgb(0, ${CurrentMinutes}, ${CurrentHours})`,
   };
 
   return (
-    <div style={style}>
-      {CurrentHours}:{CurrentMinutes}
+    <div style={dynamicBackgroundSetup}>
+      <div style={dynamicCircleObject}>
+        {CurrentHours}:{CurrentMinutes}
+      </div>
     </div>
   );
 };
